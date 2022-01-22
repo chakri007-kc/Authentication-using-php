fee0b62ae9a943184a2f3014ff70bc97608dca65
chakri007-kc <chakradhar007007kc@gmail.com>
2022-01-22 14:46:54 +0530

    first commit

---
+++ login.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="style.css">
+    <title>userRegistration</title>
+</head>
+<body>
+    <div class="container">
+        <div class="login-box">
+        <div class="row">
+            <div class="col-md-6 login-left">
+                <h2>Login here</h2>
+                <form action="validation.php" method="post">
+                    <div class="form-group">
+                        <label>Username</label>
+                        <input type="text" name="user" class="form-control" required>
+                    </div>
+                    <div class="form-group">
+                        <label>Password</label>
+                        <input type="password" name="password" class="form-control" required>
+                    </div>
+                    <button type="submit" class="btn btn-primary">Login</button>
+                </form>
+            </div>
+
+             <div class="col-md-6 login-right">
+                <h2>Registration here</h2>
+                <form action="registration.php" method="post">
+                    <div class="form-group">
+                        <label>Username</label>
+                        <input type="text" name="user" class="form-control" required>
+                    </div>
+                    <div class="form-group">
+                        <label>Password</label>
+                        <input type="password" name="password" class="form-control" required>
+                    </div>
+                    <button type="submit" class="btn btn-primary">Register</button>
+                </form>
+            </div>
+
+        </div>
+        </div>
+    </div>
+</body>
+</html>
